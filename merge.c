c08a1505bd4bff4fe297ce8b2baa3b8f37d467df
aleefa <116252564+aleefa@users.noreply.github.com>
2022-11-16 12:38:33 +0530

    Add files via upload

---
+++ merge.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+void main()
+{
+int ar1[20],ar2[20],ar3[40];
+int m,n;
+void read_array(int [],int);
+void print_array(int [],int);
+void merge(int[],int [],int [],int,int);
+printf("no.of elements in 1st array[1-20]:");
+scanf("%d",&m);
+printf("enter the elements in non-decreasing order:");
+read_array(ar1,m);
+printf("no.of elemts in 2nd array:");
+scanf("%d",&n);
+printf("enter the elements in non-decreasing order:");
+read_array(ar2,n);
+merge(ar1,ar2,ar3,m,n);
+printf("merged array:\n");
+print_array(ar3,m+n);
+}
+void read_array(int ar[],int m)
+{
+int i;
+for(i=0;i<m;i++)
+scanf("%d",&ar[i]);
+return;
+}
+void print_array(int ar[],int n)
+{
+int i;
+for(i=0;i<n;i++)
+printf("%d, ",ar[i]);
+return;
+}
+void merge(int a[],int b[],int c[],int m,int n)
+{
+int i,j,k;
+i=j=k=0;
+while(i<m&j<n)
+if(a[i]<b[j])
+{
+c[k]=a[i];
+i=i+1;
+k=k+1;
+}
+else
+{
+c[k]=b[j];
+j=j+1;
+k=k+1;
+}
+while(i<m)
+c[k++]=a[i++];
+while(j<n)
+c[k++]=b[j++];
+}
